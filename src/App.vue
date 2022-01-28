58296db077901bdcd0c062d1da3ec93d52408cc1
Daniel Kelly <me@danielkelly.io>
2022-01-28 14:38:01 -0600

    5. access state from a pinia store

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
 import { useProductStore } from "@/stores/ProductStore";
-useProductStore();
+const productStore = useProductStore();
 </script>
 
 <template>
@@ -11,7 +10,7 @@ useProductStore();
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
       />
