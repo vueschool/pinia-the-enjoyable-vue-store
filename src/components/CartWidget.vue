c7ff4b0f541cf78df86623ff489bcb71a18bcc44
Daniel Kelly <me@danielkelly.io>
2022-02-01 17:24:58 -0600

    9. dynamic getters and displaying the cart items

--- src/components/CartWidget.vue
+++ src/components/CartWidget.vue
@@ -20,14 +20,10 @@ const active = ref(false);
       <div v-if="!cartStore.isEmpty">
         <ul class="items-in-cart">
           <CartItem
-            :product="{ name: 'Dried Pineapple', price: 5 }"
-            :count="5"
-            @updateCount=""
-            @clear=""
-          />
-          <CartItem
-            :product="{ name: 'Pineapple Gum', price: 3 }"
-            :count="5"
+            v-for="(items, name) in cartStore.grouped"
+            :key="name"
+            :product="items[0]"
+            :count="cartStore.groupCount(name)"
             @updateCount=""
             @clear=""
           />
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
 export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
@@ -8,6 +9,8 @@ export const useCartStore = defineStore("CartStore", {
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
+    grouped: (state) => groupBy(state.items, (item) => item.name),
+    groupCount: (state) => (name) => state.grouped[name].length,
   },
   actions: {
     addItems(count, item) {
