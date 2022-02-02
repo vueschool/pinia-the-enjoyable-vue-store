4fb1ba9607fb8d8a4c28a2e2238068698663b01f
Daniel Kelly <me@danielkelly.io>
2022-02-02 15:14:36 -0600

    11. challenge: cart modifications

--- src/components/CartWidget.vue
+++ src/components/CartWidget.vue
@@ -25,11 +25,11 @@ const active = ref(false);
             :product="items[0]"
             :count="cartStore.groupCount(name)"
             @updateCount=""
-            @clear=""
+            @clear="cartStore.clearItem(name)"
           />
         </ul>
         <div class="flex justify-end text-2xl mb-5">
-          Total: <strong>$40</strong>
+          Total: <strong>${{ cartStore.total }}</strong>
         </div>
         <div class="flex justify-end">
           <AppButton class="secondary mr-2" @click="cartStore.$reset()"
--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -11,6 +11,7 @@ export const useCartStore = defineStore("CartStore", {
     isEmpty: (state) => state.count === 0,
     grouped: (state) => groupBy(state.items, (item) => item.name),
     groupCount: (state) => (name) => state.grouped[name].length,
+    total: (state) => state.items.reduce((p, c) => p + c.price, 0),
   },
   actions: {
     addItems(count, item) {
@@ -18,6 +19,9 @@ export const useCartStore = defineStore("CartStore", {
       for (let index = 0; index < count; index++) {
         this.items.push({ ...item });
       }
+    },
+    clearItem(itemName) {
+      this.items = this.items.filter((item) => item.name !== itemName);
     },
   },
 });
