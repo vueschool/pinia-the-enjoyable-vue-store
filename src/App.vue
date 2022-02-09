8b7129b9d5c384a2e8303d7cd339976a137b30e5
Daniel Kelly <me@danielkelly.io>
2022-02-09 17:20:08 -0600

    17 subscribe to the state

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
 import { useCartStore } from "@/stores/CartStore";
+import { ref, reactive } from "vue";
+import AppButton from "@/components/AppButton.vue";
 const productStore = useProductStore();
 const cartStore = useCartStore();
+const history = reactive([]);
+const doingHistory = ref(false);
+history.push(JSON.stringify(cartStore.$state));
+const undo = () => {
+  if (history.length === 1) return;
+  doingHistory.value = true;
+  history.pop();
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+};
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state));
+  }
+});
 cartStore.$onAction(({ name, store, args, after, onError }) => {
   if (name === "addItems") {
     after(() => {
@@ -21,6 +38,9 @@ productStore.fill();
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
